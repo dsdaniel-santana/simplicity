38d0a270a06dd876629ad920fbbb7e208fcc2a8a
dsdaniel-santana <daniel_santana_87@hotmail.com>
2023-04-25 15:31:29 -0300

    Ajustes Finais menu

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,16 @@ const linksMenu = document.querySelector(".menu");
 const icone = document.querySelector(".icone");
 
 botao.addEventListener("click", function(event){
+    /* event.preventDefault(); evita que a página recarrega */
     event.preventDefault();
     linksMenu.classList.toggle("aberto");
+
+    /* Se a classe "aberto" estiver aplicada ao linksMenu, o texto/icone para 
+    "Fechar X". Caso, contrário, mostre o texto/ícone "Menu &equiv;"  */
+    if (linksMenu.classList.contains("aberto")){
+        icone.innerHTML = "Fechar &times;";
+
+    } else{
+        icone.innerHTML = "Menu &equiv;"
+    }
 });
